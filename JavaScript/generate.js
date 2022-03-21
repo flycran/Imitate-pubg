8c3911a0609fc5b7688f5193525443e215e10cd4
cffh <cffhidol@163.com>
2022-03-21 16:11:48 +0800

    init

---
+++ JavaScript/generate.js
@@ -0,0 +1,49 @@
+window.addEventListener("DOMContentLoaded",function(){var b=document.querySelectorAll(".head_div4")[0],c=document.querySelectorAll(".head_div4")[1],d=document.querySelectorAll(".head_div4")[2],e=document.querySelectorAll(".head_div4")[3];[{name:"\u5730\u4E0B\u57CE\u4E0E\u52C7\u58EB",hr1:"https://dnf.qq.com/",hr2:"https://dnf.qq.com/web2015/down.shtml"},{name:"\u7A7F\u8D8A\u706B\u7EBF",hr1:"https://cf.qq.com/",hr2:"https://cf.qq.com/web201105/download.shtml"},{name:"FIFA Online 4",hr1:"https://fo4.qq.com/main.shtml",hr2:"https://fo4.qq.com/web201802/down.shtml"},{name:"\u82F1\u96C4\u8054\u76DFLOL",hr1:"https://lol.qq.com/main.shtml",hr2:"https://lol.qq.com/download.shtml?ADTAG=lolweb.v3"},{name:"NBA2K Online2",hr1:"https://nba2k2.qq.com/main.html",hr2:"https://nba2k2.qq.com/download.html"},{name:"\u5929\u6DAF\u660E\u6708\u5200",hr1:"https://wuxia.qq.com/",hr2:"https://wuxia.qq.com/download.shtml"}].map(function(c,a){b.insertAdjacentHTML("beforeEnd",`
+		<div id="" class="div_p" >
+			<span>${a+1}</span>
+			<span> ${c.name} </span>
+			<div id="">
+				<img src="../img/picture/paihb/A/${a}.jpg" >
+				<div id="">
+					<a href="${c.hr1}">进入官网</a>
+					<a href="${c.hr2}">下载游戏</a>
+				</div>
+			</div>
+		</div>
+		`)}),[{name:"\u5730\u4E0B\u57CE\u4E0E\u52C7\u58EB",hr1:"https://dnf.qq.com/",hr2:"https://dnf.qq.com/web2015/down.shtml"},{name:"\u7A7F\u8D8A\u706B\u7EBF",hr1:"https://cf.qq.com/",hr2:"https://cf.qq.com/web201105/download.shtml"},{name:"\u82F1\u96C4\u8054\u76DFLOL",hr1:"https://lol.qq.com/main.shtml",hr2:"https://lol.qq.com/download.shtml?ADTAG=lolweb.v3"},{name:"FIFA Online 4",hr1:"https://fo4.qq.com/main.shtml",hr2:"https://fo4.qq.com/web201802/down.shtml"},{name:"NBA2K Online2",hr1:"https://nba2k2.qq.com/main.html",hr2:"https://nba2k2.qq.com/download.html"},{name:"\u5929\u6DAF\u660E\u6708\u5200",hr1:"https://wuxia.qq.com/",hr2:"https://wuxia.qq.com/download.shtml"}].map(function(b,a){c.insertAdjacentHTML("beforeEnd",`
+		<div id="" class="div_p" >
+			<span>${a+1}</span>
+			<span> ${b.name} </span>
+			<div id="">
+				<img src="../img/picture/paihb/B/${a}.jpg" >
+				<div id="">
+					<a href="${b.hr1}">进入官网</a>
+					<a href="${b.hr2}">下载游戏</a>
+				</div>
+			</div>
+		</div>
+		`)}),[{name:"\u5984\u60F3\u5C71\u6D77",hr1:"https://shanhai.qq.com/main.html",hr2:"#"},{name:"\u4F7F\u547D\u53EC\u5524\u624B\u6E38\u56FD\u5185\u7248",hr1:"#",hr2:"#"},{name:"\u5996\u7CBE\u7684\u5C3E\u5DF4:\u529B\u91CF\u89C9\u9192",hr1:"#",hr2:"#"},{name:"\u84DD\u6708\u4F20\u59472",hr1:"#",hr2:"#"}].map(function(b,a){d.insertAdjacentHTML("beforeEnd",`
+		<div id="" class="div_p" >
+			<span>${a+1}</span>
+			<span> ${b.name} </span>
+			<div id="">
+				<img src="../img/picture/paihb/C/${a}.jpg" >
+				<div id="">
+					<a href="${b.hr1}">进入官网</a>
+					<a href="${b.hr2}">下载游戏</a>
+				</div>
+			</div>
+		</div>
+		`)}),[{name:"\u5984\u60F3\u5C71\u6D77",hr1:"#",hr2:"#"},{name:"\u7A7F\u8D8A\u706B\u7EBF\uFF1A\u67AA\u6218\u738B\u8005",hr1:"#",hr2:"#"},{name:"\u738B\u8005\u8363\u8000",hr1:"#",hr2:"#"},{name:"\u548C\u5E73\u7CBE\u82F1",hr1:"#",hr2:"#"}].map(function(b,a){e.insertAdjacentHTML("beforeEnd",`
+		<div id="" class="div_p" >
+			<span>${a+1}</span>
+			<span> ${b.name} </span>
+			<div id="">
+				<img src="../img/picture/paihb/D/${a}.jpg" >
+				<div id="">
+					<a href="${b.hr1}">进入官网</a>
+					<a href="${b.hr1}">下载游戏</a>
+				</div>
+			</div>
+		</div>
+		`)})});
